2503fd9d5515f12cacb65ffc59eda6e7f226a527
xxx1703xxx <119849514+xxx1703xxx@users.noreply.github.com>
2025-01-20 17:03:12 +0200

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using lr8.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace lr8.Controllers 
+{ 
+public class ProductController : Controller
+{
+    public IActionResult Index()
+    {
+        var products = new List<Product>
+            {
+                new Product { ID = 1, Name = "Product A", Price = 10.99M, CreatedDate = DateTime.Now },
+                new Product { ID = 2, Name = "Product B", Price = 20.49M, CreatedDate = DateTime.Now },
+                new Product { ID = 3, Name = "Product C", Price = 15.00M, CreatedDate = DateTime.Now }
+            };
+
+        return View(products);
+    }
+}
+}
---
+++ Views/Product/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lr8.Views.Product
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
